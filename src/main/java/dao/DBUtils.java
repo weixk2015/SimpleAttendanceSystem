fcb9bb0f3f59af3ef0bce283d1e77b4571f08e2d
devilpi <517348646@qq.com>
2018-01-06 16:28:43 +0800

    DBUtils

--- src/main/java/dao/DBUtils.java
+++ src/main/java/dao/DBUtils.java
@@ -9,12 +9,24 @@ import com.ibatis.common.resources.Resources;
  * Created by devilpi on 06/01/2018.
  */
 public class DBUtils {
-    static DBHelper dbHelper = new DBHelper("attendance");
+    static DBHelper dbHelper = new DBHelper("attendance_eva");
     public static ResultSet executeSql(String sql) {
-
+        ResultSet resultSet = null;
+        try {
+            Statement stmt = dbHelper.conn.createStatement();
+            resultSet = stmt.executeQuery("SELECT * FROM room");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resultSet;
     }
 
     public static void executeUpdate(String sql) {
-
+        try {
+            Statement stmt = dbHelper.conn.createStatement();
+            stmt.executeUpdate("SELECT * FROM room");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
