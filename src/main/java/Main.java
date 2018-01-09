db32d63edeacf2f4cae8ca76edb586a1c25e4a06
devilpi <517348646@qq.com>
2018-01-09 19:54:14 +0800

    DBUtils

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,96 @@
 package main.java;
 
 import main.java.controller.Controller;
-import main.java.entity.SystemManager;
+import main.java.entity.*;
+
+import java.lang.reflect.Method;
 
 import java.util.Scanner;
+
+import main.java.entity.User;
 
 /**
  * Created by devilpi on 06/01/2018.
  */
 public class Main {
-    static String info0 = "\nwelcome to simple attendance system v1.0\nPlease input your username: ";
-    static String info1 = "Please input your password: ";
+    static String action[] = {"login", "logout", "query", "modify"};
+
     static Controller controller = new Controller();
+    static User user = null;
 
     public static void main(String[] args) {
-        test();
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            System.out.print("Please input what you want to do: ");
+            String act = sc.nextLine();
+            boolean flag = false;
+            for(int i = 0; i < action.length; i ++) {
+                if(act.equals(action[i])) {
+                    flag = true;
+                    Class<Main> classType = Main.class;
+                    try {
+                        Method method = classType.getDeclaredMethod(action[i], null);
+                        method.invoke(classType, null);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+            }
+            if(!flag) System.out.println("Sorry, your action is illegal!");
+        }
     }
 
-    public static void control() {
-        System.out.print(info0);
+    public static void login() {
+        if(user != null) {
+            System.out.println("You have already log in!");
+            return;
+        }
+
+        System.out.print("\n" +
+                "welcome to simple attendance system v1.0\n" +
+                "Please input your username: ");
         Scanner sc = new Scanner(System.in);
         int employeeId = Integer.parseInt(sc.nextLine());
-        System.out.print(info1);
+        System.out.print("Please input your password: ");
         String password = sc.nextLine();
-        controller.login(employeeId, password);
-    }
 
-    public static void test() {
-        SystemManager systemManager = new SystemManager();
+        User tmp = new User();
         try {
-            systemManager.showUsers();
+            boolean flag = tmp.login(employeeId, password);
+            if(flag) {
+                System.out.println("You have log in!");
+                tmp.log("login", "first log");
+                switch (tmp.getType()) {
+                    case EMPLOYEE: user = new Employee(tmp);
+                        break;
+                    case HR: user = new HRmanager();
+                        break;
+                    case MANAGER: user = new DepartmentManager();
+                        break;
+                    case ADMIN: user = new SystemManager();
+                        break;
+                    default: break;
+                }
+            }
+            else {
+                System.out.println("employeeId or password is wrong!");
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+
+
     }
+
+    public static void logout() {
+        if(user == null) {
+            System.out.println("You have not log in!");
+            return;
+        }
+        user = null;
+    }
+
+
 }
--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -14,7 +14,7 @@ import java.sql.ResultSet;
  */
 public class User {
 
-    enum TYPE {EMPLOYEE,HR,MANAGER,ADMIN};
+    public enum TYPE {EMPLOYEE,HR,MANAGER,ADMIN};
     protected int employeeId;
     protected int age;
     protected String password;
